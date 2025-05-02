55131af8d633bc6c9b27129f802d1f21c8a614c6
zairduarte22 <67618103+zairduarte22@users.noreply.github.com>
2025-05-02 17:02:57 -0400

    Update facturas.py

--- views/facturas.py
+++ views/facturas.py
@@ -656,277 +656,288 @@ fecha_actual = datetime.now().strftime("%d/%m/%Y")
 usuario = "John Doe"
 total = 1250.75
 
-html_content = """
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        @page {
-            size: 205mm 148mm;
-            margin: 0;
-            padding: 0;
-        }
-        body {
-            width: 205mm;
-            margin: 0;
-            padding: 0;
-            padding: 4mm;
-            font-family: Arial;
-            font-size: 12.5pt;
-            position: relative;
-        }
-        .pagina {
-            width: 205mm;
-            height: 148mm;
-            position: relative;
-            page-break-after: always;
-            overflow: hidden;
-        }
-        .pagina:last-child {
-            page-break-after: auto;
-        }
-        .date {
-            position: absolute;
-            top: 31mm;
-            right: 152mm;
-        }
-        .name {
-            position: absolute;
-            top: 43mm;
-            left: 66mm;
-            font-size: 13.5pt;
-        }
-        .address {
-            position: absolute;
-            top: 49mm;
-            left: 50mm;
-        }
-        .id {
-            position: absolute;
-            top: 54mm;
-            left: 150mm;
-            /* Prueba añadiendo !important para la impresión */
-            /* position: absolute !important;
-            top: 60mm !important;
-            left: 148mm !important; */
-        }
-
-        .item-container {
-            position: absolute;
-            top: 67mm;
-            left: 18mm;
-            width: 180mm;
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .item-description {
-            width: 104mm;
-            text-align: left;
-            word-wrap: break-word;
-            overflow-wrap: break-word;
-            white-space: normal;
-            /* background-color: rgba(255,0,0,0.1); /* Solo para visualización */
-        }
-
-        .item-amount {
-            width: 32mm;
-            text-align: right;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-            /* background-color: rgba(0,255,0,0.1); /* Solo para visualización */
-            /* Prueba añadiendo !important para la impresión */
-            /* display: flex !important;
-            flex-direction: column !important;
-            justify-content: flex-end !important; */
-        }
-
-        .total1 {
-            position: absolute;
-            top: 121mm;
-            right: 20mm;
-            text-align: right;
-        }
-        .dash1 {
-            position: absolute;
-            top: 126mm;
-            right: 20mm;
-            text-align: right;
-        }
-        .dash2 {
-            position: absolute;
-            top: 131mm;
-            right: 20mm;
-            text-align: right;
-        }
-        .final-total {
-            position: absolute;
-            top: 136mm;
-            right: 20mm;
-            text-align: right;
-        }
-        
-        .date2 {
-            position: absolute;
-            top: 48mm;
-            right: 143mm;
-        }
-        
-        .name2 {
-            position: absolute;
-            top: 54mm;
-            left: 55mm;
-            font-size: 13.5pt;
-        }
-        
-        .address2 {
-            position: absolute;
-            top: 59mm;
-            left: 38mm;
-        }
-        
-        .id2 {
-            position: absolute;
-            top: 65mm;
-            left: 157mm;
-            /* Prueba añadiendo !important para la impresión */
-            /* position: absolute !important;
-            top: 60mm !important;
-            left: 148mm !important; */
-        }
-
-        .item-container2 {
-            position: absolute;
-            top: 78mm;
-            left: 27mm;
-            width: 168mm;
-            display: flex;
-            justify-content: space-between;
-            font-size: 13.5pt;
-        }
-
-        .item-description2 {
-            width: 104mm;
-            text-align: left;
-            word-wrap: break-word;
-            overflow-wrap: break-word;
-            white-space: normal;
-            /* background-color: rgba(255,0,0,0.1); /* Solo para visualización */
-        }
-
-        .item-amount2 {
-            width: 28mm;
-            text-align: center;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-            /* background-color: rgba(0,255,0,0.1); /* Solo para visualización */
-            /* Prueba añadiendo !important para la impresión */
-            /* display: flex !important;
-            flex-direction: column !important;
-            justify-content: flex-end !important; */
-        }
-
-        .total12 {
-            position: absolute;
-            top: 120mm;
-            right: 18mm;
-            text-align: right;
-        }
-        .dash12 {
-            position: absolute;
-            top: 128mm;
-            right: 18mm;
-            text-align: right;
-        }
-        .dash22 {
-            position: absolute;
-            top: 135mm;
-            right: 18mm;
-            text-align: right;
-        }
-        .final-total2 {
-            position: absolute;
-            top: 141mm;
-            right: 18mm;
-            text-align: right;
-        }
-
-
-        /* Estilos específicos para la impresión (prueba aquí) */
-        @media print {
-            /* Intenta forzar la visualización */
+def invoice(date: datatime, name: str, adress: str, id: str, month: str, monto: float):
+    day = date.day()
+    month_date = date.month()
+    year = date.year()
+    name = name
+    adress = adress
+    id = id
+    month = month
+    monto = monto
+    monto_ugavi = monto * 0.6
+    monto_fondo = monto * 0.2
+    total1 = monto_ugavi + monto_fondo
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            @page {
+                size: 205mm 148mm;
+                margin: 0;
+                padding: 0;
+            }
+            body {
+                width: 205mm;
+                margin: 0;
+                padding: 0;
+                padding: 4mm;
+                font-family: Arial;
+                font-size: 12.5pt;
+                position: relative;
+            }
+            .pagina {
+                width: 205mm;
+                height: 148mm;
+                position: relative;
+                page-break-after: always;
+                overflow: hidden;
+            }
+            .pagina:last-child {
+                page-break-after: auto;
+            }
+            .date {
+                position: absolute;
+                top: 31mm;
+                right: 152mm;
+            }
+            .name {
+                position: absolute;
+                top: 43mm;
+                left: 66mm;
+                font-size: 13.5pt;
+            }
+            .address {
+                position: absolute;
+                top: 49mm;
+                left: 50mm;
+            }
             .id {
+                position: absolute;
+                top: 54mm;
+                left: 150mm;
+                /* Prueba añadiendo !important para la impresión */
                 /* position: absolute !important;
                 top: 60mm !important;
                 left: 148mm !important; */
-                color: black !important; /* Asegúrate de que el color no sea blanco o transparente */
             }
+    
+            .item-container {
+                position: absolute;
+                top: 67mm;
+                left: 18mm;
+                width: 180mm;
+                display: flex;
+                justify-content: space-between;
+            }
+    
+            .item-description {
+                width: 104mm;
+                text-align: left;
+                word-wrap: break-word;
+                overflow-wrap: break-word;
+                white-space: normal;
+                /* background-color: rgba(255,0,0,0.1); /* Solo para visualización */
+            }
+    
             .item-amount {
+                width: 32mm;
+                text-align: right;
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-end;
+                /* background-color: rgba(0,255,0,0.1); /* Solo para visualización */
+                /* Prueba añadiendo !important para la impresión */
                 /* display: flex !important;
                 flex-direction: column !important;
                 justify-content: flex-end !important; */
-                color: black !important; /* Asegúrate de que el color no sea blanco o transparente */
             }
-
-            /* Prueba con un borde para ver si el elemento está ahí */
-            /* .id { border: 1px solid black !important; }
-            .item-amount { border: 1px solid blue !important; } */
-        }
-    </style>
-</head>
-<body>
-     <div class="pagina">
-        <div class="date">31 &emsp;12 &emsp;2025</div>
     
-        <div class="name">MARIO SAAB</div>
-        <div class="address">URB EL VALLE</div>
-        <div class="id">V-127582390</div>
+            .total1 {
+                position: absolute;
+                top: 121mm;
+                right: 20mm;
+                text-align: right;
+            }
+            .dash1 {
+                position: absolute;
+                top: 126mm;
+                right: 20mm;
+                text-align: right;
+            }
+            .dash2 {
+                position: absolute;
+                top: 131mm;
+                right: 20mm;
+                text-align: right;
+            }
+            .final-total {
+                position: absolute;
+                top: 136mm;
+                right: 20mm;
+                text-align: right;
+            }
+            
+            .date2 {
+                position: absolute;
+                top: 48mm;
+                right: 143mm;
+            }
+            
+            .name2 {
+                position: absolute;
+                top: 54mm;
+                left: 55mm;
+                font-size: 13.5pt;
+            }
+            
+            .address2 {
+                position: absolute;
+                top: 59mm;
+                left: 38mm;
+            }
+            
+            .id2 {
+                position: absolute;
+                top: 65mm;
+                left: 157mm;
+                /* Prueba añadiendo !important para la impresión */
+                /* position: absolute !important;
+                top: 60mm !important;
+                left: 148mm !important; */
+            }
     
-        <div class="item-container">
-            <div class="item-description">
-                CANCELACIÓN DEL 20% POR CUOTA CORRESPONDIENTE A ENERO 2024 CON UN TEXTO MÁS LARGO QUE GENERA SALTO DE LÍNEA
+            .item-container2 {
+                position: absolute;
+                top: 78mm;
+                left: 27mm;
+                width: 168mm;
+                display: flex;
+                justify-content: space-between;
+                font-size: 13.5pt;
+            }
+    
+            .item-description2 {
+                width: 104mm;
+                text-align: left;
+                word-wrap: break-word;
+                overflow-wrap: break-word;
+                white-space: normal;
+                /* background-color: rgba(255,0,0,0.1); /* Solo para visualización */
+            }
+    
+            .item-amount2 {
+                width: 28mm;
+                text-align: center;
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-end;
+                /* background-color: rgba(0,255,0,0.1); /* Solo para visualización */
+                /* Prueba añadiendo !important para la impresión */
+                /* display: flex !important;
+                flex-direction: column !important;
+                justify-content: flex-end !important; */
+            }
+    
+            .total12 {
+                position: absolute;
+                top: 120mm;
+                right: 18mm;
+                text-align: right;
+            }
+            .dash12 {
+                position: absolute;
+                top: 128mm;
+                right: 18mm;
+                text-align: right;
+            }
+            .dash22 {
+                position: absolute;
+                top: 135mm;
+                right: 18mm;
+                text-align: right;
+            }
+            .final-total2 {
+                position: absolute;
+                top: 141mm;
+                right: 18mm;
+                text-align: right;
+            }
+    
+    
+            /* Estilos específicos para la impresión (prueba aquí) */
+            @media print {
+                /* Intenta forzar la visualización */
+                .id {
+                    /* position: absolute !important;
+                    top: 60mm !important;
+                    left: 148mm !important; */
+                    color: black !important; /* Asegúrate de que el color no sea blanco o transparente */
+                }
+                .item-amount {
+                    /* display: flex !important;
+                    flex-direction: column !important;
+                    justify-content: flex-end !important; */
+                    color: black !important; /* Asegúrate de que el color no sea blanco o transparente */
+                }
+    
+                /* Prueba con un borde para ver si el elemento está ahí */
+                /* .id { border: 1px solid black !important; }
+                .item-amount { border: 1px solid blue !important; } */
+            }
+        </style>
+    </head>
+    <body>
+         <div class="pagina">
+            <div class="date">{day} &emsp;{month_date} &emsp;{year}</div>
+        
+            <div class="name">{name}</div>
+            <div class="address">{adress}</div>
+            <div class="id">{id}</div>
+        
+            <div class="item-container">
+                <div class="item-description">
+                    CANCELACIÓN DEL 20% POR CUOTA CORRESPONDIENTE A ENERO 2024 CON UN TEXTO MÁS LARGO QUE GENERA SALTO DE LÍNEA
+                </div>
+                <div class="item-amount">
+                    144.00
+                </div>
             </div>
-            <div class="item-amount">
-                144.00
-            </div>
+        
+            <div class="total1">576.00</div>
+            <div class="dash1">-</div>
+            <div class="dash2">-</div>
+            <div class="final-total">576.00</div>
         </div>
     
-        <div class="total1">576.00</div>
-        <div class="dash1">-</div>
-        <div class="dash2">-</div>
-        <div class="final-total">576.00</div>
-    </div>
-
-    <!-- SEGUNDA PÁGINA (MONTOS ACTUALIZADOS) -->
-    <div class="pagina">
-        <div class="date2">31 &emsp;12 &emsp;2025</div>
-    
-        <div class="name2">MARIO ROMERO</div>
-        <div class="address2">URB EL VALLE</div>
-        <div class="id2">V-127582390</div>
-    
-        <div class="item-container2">
-            <div class="item-description2">
-                CANCELACIÓN DEL 20% POR CUOTA CORRESPONDIENTE A ENERO 2024 CON UN TEXTO MÁS LARGO QUE GENERA SALTO DE LÍNEA
+        <!-- SEGUNDA PÁGINA (MONTOS ACTUALIZADOS) -->
+        <div class="pagina">
+            <div class="date2">31 &emsp;12 &emsp;2025</div>
+        
+            <div class="name2">MARIO ROMERO</div>
+            <div class="address2">URB EL VALLE</div>
+            <div class="id2">V-127582390</div>
+        
+            <div class="item-container2">
+                <div class="item-description2">
+                    CANCELACIÓN DEL 20% POR CUOTA CORRESPONDIENTE A ENERO 2024 CON UN TEXTO MÁS LARGO QUE GENERA SALTO DE LÍNEA
+                </div>
+                <div class="item-amount2">
+                    144.00
+                </div>
             </div>
-            <div class="item-amount2">
-                144.00
-            </div>
+        
+            <div class="total12">576.00</div>
+            <div class="dash12">-</div>
+            <div class="dash22">-</div>
+            <div class="final-total2">576.00</div>
         </div>
-    
-        <div class="total12">576.00</div>
-        <div class="dash12">-</div>
-        <div class="dash22">-</div>
-        <div class="final-total2">576.00</div>
-    </div>
-</body>
-</html>
-"""
+    </body>
+    </html>
+    """
 
 
 # Configurar el sistema de impresión
 setup_printing(html_content=html_content)
 
-st.write("Use el botón flotante en la esquina inferior derecha para imprimir")
