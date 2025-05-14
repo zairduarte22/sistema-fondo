cef9307eede4bc4d50334ab3e4b4eccc2c8f633b
zairduarte22 <zairduarte22@gmail.com>
2025-05-14 08:02:07 -0400

    Cambios en el reporte de miembros

--- views/miembros.py
+++ views/miembros.py
@@ -461,7 +461,7 @@ with header:
         if cobrar:
             cobranza(miembros_completo)
     with col5:
-        pdf_buffer = generar_informe_pdf_miembros(miembros_completo)
+        pdf_buffer = generar_informe_pdf_miembros(miembros_completo, "ESTADO")
         st.download_button(
             label=":material/download: Informe",
             data=pdf_buffer,
