c2c766dfbfaa9286b374b888e24d3637819d199a
zairduarte22 <67618103+zairduarte22@users.noreply.github.com>
2025-05-19 11:37:30 -0400

    Update miembros.py

--- views/miembros.py
+++ views/miembros.py
@@ -332,6 +332,7 @@ def eliminar_datos(tabla, id_miembro):
 
 @st.dialog('Información Adicional', width="large")
 def informacion_miembro(num_indice: int, dataframe1: pd.DataFrame, dataframe2: pd.DataFrame):
+    print(dataframe2)
     id_miembro = dataframe2.loc[num_indice, "Cod."]
     info = dataframe1[dataframe1["ID_MIEMBRO"] == id_miembro]
     info = info.iloc
