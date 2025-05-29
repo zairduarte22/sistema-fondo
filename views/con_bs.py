3a93bf8ed128c798f93a64ea18ca274a3d26abf0
zairduarte22 <zairduarte22@gmail.com>
2025-05-29 10:44:12 -0400

    Ingresar datos bancarios

--- views/con_bs.py
+++ views/con_bs.py
@@ -177,10 +177,10 @@ def cargar_movimientos_csv():
                 # Convertir FECHA al formato correcto
                 try:
                     df["FECHA"] = pd.to_datetime(df["FECHA"], format="%d/%m/%Y").dt.date
-                    df["INGRESO"] = df["INGRESO"].replace('.', '', regex=True)
+                    df["INGRESO"] = df["INGRESO"].replace('.', ',', regex=True)
                     df["INGRESO"] = df["INGRESO"].replace(',', '.', regex=True)
                     df["INGRESO"] = pd.to_numeric(df["INGRESO"], errors='coerce').fillna(0.0)
-                    df["EGRESO"] = df["EGRESO"].replace('.', '', regex=True)
+                    df["EGRESO"] = df["EGRESO"].replace('.', ',', regex=True)
                     df["EGRESO"] = df["EGRESO"].replace(',', '.', regex=True)
                     df["EGRESO"] = pd.to_numeric(df["EGRESO"], errors='coerce').fillna(0.0)
                 except Exception:
