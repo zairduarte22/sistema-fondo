0e7e7bb5ba9a05a386fa96496a7c03441421d56e
zairduarte22 <67618103+zairduarte22@users.noreply.github.com>
2025-06-03 08:17:51 -0400

    Update bcv_tasa.py

--- utils/bcv_tasa.py
+++ utils/bcv_tasa.py
@@ -7,7 +7,7 @@ def tasa_bs():
         response = requests.get(url)  # Realiza la solicitud GET
         response.raise_for_status()  # Levanta una excepción para códigos de estado HTTP no exitosos (4xx, 5xx)
         data = response.json()
-        tasa = data["monitors"]["usd"]
+        tasa = data["monitors"]["usd"]["price"]
         return tasa
     except requests.exceptions.RequestException as e:
         print(f"Error al realizar la solicitud: {e}")
