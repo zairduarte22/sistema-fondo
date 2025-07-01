aa9096008de136acb4f5c1c051fccc5af93aad32
zairduarte22 <67618103+zairduarte22@users.noreply.github.com>
2025-07-01 12:05:48 +0000

    Debug tasa

--- utils/bcv_tasa.py
+++ utils/bcv_tasa.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import streamlit as st
 
 def tasa_bs():
     url = "https://pydolarve.org/api/v2/dollar?page=bcv"
@@ -8,6 +9,7 @@ def tasa_bs():
         response.raise_for_status()  # Levanta una excepción para códigos de estado HTTP no exitosos (4xx, 5xx)
         data = response.json()
         tasa = data["monitors"]["usd"]["price"]
+        st.toast("Obteniendo tasa del Dolar...")
         return tasa
     except requests.exceptions.RequestException as e:
         print(f"Error al realizar la solicitud: {e}")
