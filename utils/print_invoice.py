c55461706a3b4ec770ef998f157d33d79b52088b
zairduarte22 <67618103+zairduarte22@users.noreply.github.com>
2025-09-15 14:21:04 -0400

    Update print_invoice.py

--- utils/print_invoice.py
+++ utils/print_invoice.py
@@ -169,7 +169,7 @@ def invoice_model(date: datetime, name: str, adress: str, id: str, month: str, m
                 .date2 {{
                     position: absolute;
                     top: 49mm;
-                    right: 135mm;
+                    right: 158mm;
                 }}
                 
                 .name2 {{
